687c5090ab1e7a361227f0636bb3698f10084a07
prashant naik <prasnaik@in.ibm.com>
2016-11-22 14:57:47 +0000

    Modified PHP info file

--- phpinfo.php
+++ phpinfo.php
@@ -1,6 +1,13 @@
-<?php
-
-// Show all information, defaults to INFO_ALL
-phpinfo();
-
-?>
+<html>
+   <head>
+      <title>Online PHP Script Execution</title>      
+   </head>
+   
+   <body>
+      
+      <?php
+         echo "<h1>Hello, PHP!</h1>";
+      ?>
+   
+   </body>
+</html>
